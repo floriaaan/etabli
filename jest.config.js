a2d09fd9fa721c85647c27dbd91110d1139ca300
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-08-04 19:36:42 +0200

    ci(test): add codecov support

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,6 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
 
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
   moduleDirectories: ["node_modules", __dirname],
   //ignore node_modules and dist
