11b3cd87fec63c0a168b4b5b42e4ab7f4a7f51f5
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-08-06 11:28:21 +0200

    feat(coords): add coords prop to entities

--- core/classes/entities/Player.ts
+++ core/classes/entities/Player.ts
@@ -1,13 +1,23 @@
 import { Entity } from "@etabli/classes/entities/Entity";
+import { entities } from "@etabli/resources/entities";
+import { Coordinates } from "@etabli/types/Coordinates";
 
 class Player extends Entity {
   public socketId: string | undefined;
+
+
+  public spawnPoint: Coordinates = [0, 0, 0];
+
+  public xp: number = 0;
 
   constructor(name: string) {
     super("player", name);
   }
 
-  public xp: number = 0;
+  public respawn() {
+    this.health = entities.player.health;
+    this.position = this.spawnPoint;
+  }
 }
 
 export { Player };
